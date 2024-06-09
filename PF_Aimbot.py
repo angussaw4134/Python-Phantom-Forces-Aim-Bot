f946267caae06749536f3a2ebb02837d7c34ff44
angussaw4134 <117689333+angussaw4134@users.noreply.github.com>
2024-06-09 17:49:58 +1000

    Update PF_Aimbot.py

--- PF_Aimbot.py
+++ PF_Aimbot.py
@@ -12,7 +12,7 @@
 # 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # 
-Monitor_Size = [2560, 1440] # Set This to you Monitor Size [Width, Height] in pixels. 
+Monitor_Size = [1920, 1080] # Set This to you Monitor Size [Width, Height] in pixels. 
 # 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 import numpy as np
